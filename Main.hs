1d2a00d2c038ba995c6f1309f77de54df8fd0974
�Jaime Leon� <98853831+LeJammes454@users.noreply.github.com>
2023-03-23 19:28:20 +0000

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,13 @@
+
+main = do
+  putStrLn "Hola mundo"
+  
+  putStrLn "La suma es "
+  --print (suma 5 8)
+  putStrLn "La resta es "
+  --print (resta 5 8)
+  putStrLn "La multiplicacion es "
+  --print (multiplicacion 5 8)
+  putStrLn "La divicion es "
+  --print (divicion 5 8)
+
---
+++ aritmetica.hs
@@ -0,0 +1,9 @@
+-- Funciones aritmeticas basicas en haskell
+
+suma a b = a + b
+
+resta a b = a - b
+
+multiplicacion a b = a * b
+
+divicion a b = a / b
